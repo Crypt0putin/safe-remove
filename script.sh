1bbd7f44fa714625c185c196e6772870a24dc692
Crypt0putin <crypt0putin@protonmail.com>
2020-10-15 07:14:23 +0000

    Added moving gzip file to RECYCLE directory

--- script.sh
+++ script.sh
@@ -1,1 +1,13 @@
-realpath
+rp=$(realpath $1)
+gzip $rp
+
+if [ -d "~/RECYCLE" ]
+    then
+    echo "Check folder - RECYCLE succesfully!"
+  else
+    echo "Folder RECYCLE not exist. Create..."
+    /usr/bin/mkdir -p ~/RECYCLE
+fi
+
+mv $1.gz ~/RECYCLE
+echo "Done!"
