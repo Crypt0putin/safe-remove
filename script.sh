16cc6d2cd3263efdc031586ead97f282032613dd
Crypt0putin <crypt0putin@protonmail.com>
2020-10-15 08:56:54 +0000

    Added checking the time after removing the file

--- script.sh
+++ script.sh
@@ -1,13 +1,23 @@
 rp=$(realpath $1)
 gzip $rp
 
-if [ -d "~/RECYCLE" ]
-    then
-    echo "Check folder - RECYCLE succesfully!"
-  else
-    echo "Folder RECYCLE not exist. Create..."
+if ! [ -d "~/RECYCLE" ]
+then
+    echo "Проверка папки ~/RECYCLE..."
+else
+    echo "Папки RECYCLE не существует. Создаем..."
     /usr/bin/mkdir -p ~/RECYCLE
 fi
 
 mv $1.gz ~/RECYCLE
-echo "Done!"
+echo "Готово!"
+
+diff=$(($(date +%s)-$(stat -c %Y ~/RECYCLE/$1.gz)))
+day_secs=86400
+day_diff=$(($diff/$day_secs))
+echo "Через 7 дней файл будет удален из козины. На данный момент прошло" $day_diff "дней..."
+
+if $3 > 7
+then
+    rm ~/RECYCLE/$1.gz
+fi
